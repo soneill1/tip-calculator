8cafccf47b2cc93612ac4882d168e19b926b2de0
Sean O'Neill <bryte.app@gmail.com>
2018-01-19 11:38:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Code goes here
 
-function myFunction() {
-  var x = document.getElementById("txt1").value;
-  var result = x*1.20;
-  document.getElementById("answer").innerHTML = result + "%";
+
+
+
+function calculateTipAmount() {
+  var billPrice = document.getElementById("billPrice").value;
+  var slider = document.getElementById("mySlider").value;
+  var tipAmount = parseFloat("1." + slider);
+  var result = (billPrice * tipAmount).toFixed(2);
+  
+  document.getElementById("tip").innerHTML = slider + "%";
+  document.getElementById("total").innerHTML = "$" + result
+  document.getElementById("amount").innerHTML = (result - billPrice).toFixed(2);
+}
+
+function updateSliderValue() {
+  var slider = document.getElementById("mySlider");
+  var result = document.getElementById("result");
+  result.innerHTML = slider.value;
 }
