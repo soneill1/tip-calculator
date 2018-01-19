a8a1606afb23c1bf568bcb9aa696d5b8f6eb1d21
Sean O'Neill <bryte.app@gmail.com>
2018-01-18 19:09:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Code goes here
+
+function myFunction() {
+  var x = document.getElementById("txt1").value;
+  var result = x*1.20;
+  document.getElementById("answer").innerHTML = result + "%";
+}
