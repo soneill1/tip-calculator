1f50e9ea609d0b734bb52e666e9b32ff3c539cb9
Sean O'Neill <bryte.app@gmail.com>
2018-01-19 15:46:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,15 @@
 function calculateTipAmount() {
   var billPrice = document.getElementById("billPrice").value;
   var slider = document.getElementById("mySlider").value;
+  
+  if (slider < 10) {
+    slider = "0" + slider;
+  }
+  
   var tipAmount = parseFloat("1." + slider);
   var result = (billPrice * tipAmount).toFixed(2);
   
-  document.getElementById("tip").innerHTML = slider + "%";
+  document.getElementById("tip").innerHTML = parseInt(slider, 10) + "%";
   document.getElementById("total").innerHTML = "$" + result
   document.getElementById("amount").innerHTML = (result - billPrice).toFixed(2);
 }
