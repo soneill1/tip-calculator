7f2654894eecbabdda8d6e08a3f795fc7f851409
Sean O'Neill <bryte.app@gmail.com>
2018-01-20 14:04:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,19 @@
 function calculateTipAmount() {
   var billPrice = document.getElementById("billPrice").value;
   var slider = document.getElementById("mySlider").value;
-  
+  var guests = document.getElementById("guests").value;
+
   if (slider < 10) {
     slider = "0" + slider;
   }
-  
+
   var tipAmount = parseFloat("1." + slider);
   var result = (billPrice * tipAmount).toFixed(2);
-  
+
   document.getElementById("tip").innerHTML = parseInt(slider, 10) + "%";
   document.getElementById("total").innerHTML = "$" + result
   document.getElementById("amount").innerHTML = (result - billPrice).toFixed(2);
+  document.getElementById("perGuestTotal").innerHTML = (result / guests).toFixed(2);
 }
 
 function updateSliderValue() {
@@ -24,3 +26,50 @@ function updateSliderValue() {
   var result = document.getElementById("result");
   result.innerHTML = slider.value + "%";
 }
+
+jQuery(document).ready(function() {
+  // This button will increment the value
+  $('.qtyplus').click(function(e) {
+    // Stop acting like a button
+    e.preventDefault();
+    // Get the field name
+    fieldName = $(this).attr('field');
+    // Get its current value
+    var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+    // If is not undefined
+    if (!isNaN(currentVal)) {
+      // Increment only if value is < 20
+      if (currentVal < 50) {
+        $('input[name=' + fieldName + ']').val(currentVal + 1);
+        $('.qtyminus').val("-").removeAttr('style');
+      } else {
+        $('.qtyplus').val("+").css('color', '#aaa');
+        $('.qtyplus').val("+").css('cursor', 'not-allowed');
+      }
+    } else {
+      // Otherwise put a 0 there
+      $('input[name=' + fieldName + ']').val(1);
+
+    }
+  });
+  // This button will decrement the value till 0
+  $(".qtyminus").click(function(e) {
+    // Stop acting like a button
+    e.preventDefault();
+    // Get the field name
+    fieldName = $(this).attr('field');
+    // Get its current value
+    var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+    // If it isn't undefined or its greater than 0
+    if (!isNaN(currentVal) && currentVal > 1) {
+      // Decrement one only if value is > 1
+      $('input[name=' + fieldName + ']').val(currentVal - 1);
+      $('.qtyplus').val("+").removeAttr('style');
+    } else {
+      // Otherwise put a 0 there
+      $('input[name=' + fieldName + ']').val(1);
+      $('.qtyminus').val("-").css('color', '#aaa');
+      $('.qtyminus').val("-").css('cursor', 'not-allowed');
+    }
+  });
+});
